574d7988b7d772a843dd4e4001ac6c8d0e16559e
Scrounger <scrounger@gmx.net>
2022-05-06 17:28:47 +0200

    fallback improvments

--- src/modules/exif.ts
+++ src/modules/exif.ts
@@ -2,6 +2,7 @@ import * as exifr from "exifr";
 import { GlobalHelper } from "./global-helper";
 import { ExifImage } from "exif";
 import moment from "moment";
+import gpsCoordParser from "coordinate-parser";
 
 export interface exifinfo {
 	info1: string;
@@ -14,31 +15,80 @@ export interface exifinfo {
 
 export async function getPictureInformation(Helper: GlobalHelper, file: string | Buffer): Promise<exifinfo | null> {
 	try {
-		const PictureInfo = await exifr.parse(file, ["XPTitle", "XPComment", "XPSubject", "DateTimeOriginal", "latitude", "longitude"]);
+		const PictureInfo = await exifr.parse(file, ["XPTitle", "XPComment", "XPSubject", "DateTimeOriginal", "CreateDate", "latitude", "longitude"]);
 		const GpsInfo = await exifr.gps(file);
 
-		let date = null;
-
-		if (!PictureInfo || !PictureInfo["DateTimeOriginal"] || !GpsInfo || !GpsInfo.latitude || !GpsInfo.longitude) {			
-			let fallbackData = await getExifFallback(Helper, file);
-			Helper.ReportingInfo("Info", "Adapter", file + ": trying to get EXIF Data using fallback lib");
-
-			if (fallbackData && fallbackData.exif) {
-				date = fallbackData && fallbackData.exif && fallbackData.exif.DateTimeOriginal ? new Date(moment(fallbackData.exif.DateTimeOriginal, 'YYYY:MM:DD HH:mm:ss').toString()) : null;
-			}
-		}
+		let fallbackData = await getFallbackData(Helper, file, PictureInfo, GpsInfo);
 
 		return {
 			info1: PictureInfo && PictureInfo["XPTitle"] ? PictureInfo["XPTitle"] : "",
 			info2: PictureInfo && PictureInfo["XPSubject"] ? PictureInfo["XPSubject"] : "",
 			info3: PictureInfo && PictureInfo["XPComment"] ? PictureInfo["XPComment"] : "",
-			date: PictureInfo && PictureInfo["DateTimeOriginal"] ? new Date(PictureInfo["DateTimeOriginal"]) : date,
-			latitude: GpsInfo && GpsInfo.latitude ? GpsInfo.latitude : null,
-			longitude: GpsInfo && GpsInfo.longitude ? GpsInfo.longitude : null
+			date: PictureInfo && PictureInfo["DateTimeOriginal"] ? new Date(PictureInfo["DateTimeOriginal"]) : PictureInfo && PictureInfo["CreateDate"] ? new Date(PictureInfo["CreateDate"]) : fallbackData.date,
+			latitude: GpsInfo && GpsInfo.latitude ? GpsInfo.latitude : fallbackData.latitude,
+			longitude: GpsInfo && GpsInfo.longitude ? GpsInfo.longitude : fallbackData.longitude
 		};
 	} catch (error) {
 		Helper.ReportingError(error as Error, "Unknown Error", "exifr", "getPictureInformation");
 		return null;
+	}
+}
+
+export interface fallbackLib {
+	date: Date | null;
+	latitude: number | null;
+	longitude: number | null;
+}
+
+async function getFallbackData(Helper: GlobalHelper, file: string | Buffer, PictureInfo: any, GpsInfo: any): Promise<fallbackLib> {
+	let fallbackDate = null;
+	let fallbackLatitude = null;
+	let fallbackLongitude = null;
+
+	if (!PictureInfo || !PictureInfo["DateTimeOriginal"] || !GpsInfo || !GpsInfo.latitude || !GpsInfo.longitude) {
+		let fallbackData = await getExifFallback(Helper, file);
+
+		if (fallbackData) {
+			if (!PictureInfo || !PictureInfo["DateTimeOriginal"]) {
+				if (fallbackData.exif && fallbackData.exif.DateTimeOriginal) {
+					Helper.ReportingInfo("Debug", "Adapter", `using fallback lib: file: '${file}', DateTimeOriginal: '${fallbackData.exif.DateTimeOriginal}'`);
+					fallbackDate = fallbackData.exif && fallbackData.exif.DateTimeOriginal ? new Date(moment(fallbackData.exif.DateTimeOriginal, 'YYYY:MM:DD HH:mm:ss').toString()) : null;
+				}
+			}
+
+			if (fallbackDate === null && fallbackData.gps && fallbackData.gps.GPSDateStamp) {
+				Helper.ReportingInfo("Debug", "Adapter", `using fallback lib: file: '${file}', GPSDateStamp: '${fallbackData.gps.GPSDateStamp}'`);
+				fallbackDate = new Date(moment(fallbackData.gps.GPSDateStamp, 'YYYY:MM:DD').toString());
+			}
+
+			if (!GpsInfo || !GpsInfo.latitude || !GpsInfo.longitude) {
+				if (fallbackData.gps && fallbackData.gps.GPSLatitudeRef && fallbackData.gps.GPSLatitude && fallbackData.gps.GPSLongitudeRef && fallbackData.gps.GPSLongitude) {
+					let latitudeTmp = `${fallbackData.gps.GPSLatitude[0]}:${fallbackData.gps.GPSLatitude[1]}:${fallbackData.gps.GPSLatitude[2]}${fallbackData.gps.GPSLatitudeRef}`
+					let longitudeTmp = `${fallbackData.gps.GPSLongitude[0]}:${fallbackData.gps.GPSLongitude[1]}:${fallbackData.gps.GPSLongitude[2]}${fallbackData.gps.GPSLongitudeRef}`
+
+					let coordTmp = `${latitudeTmp} ${longitudeTmp}`;
+
+					Helper.ReportingInfo("Debug", "Adapter", `using fallback lib: file: '${file}', Coordinates: '${coordTmp}'`);
+
+					try {
+						let position = new gpsCoordParser(coordTmp);
+
+						fallbackLatitude = position.getLatitude();
+						fallbackLongitude = position.getLongitude();
+					} catch (err) {
+						Helper.ReportingError(err as Error, "Unknown Error", "gpsCoordParser", "getFallbackData");
+					}
+				}
+			}
+
+
+		}
+	}
+
+	return {
+		date: fallbackDate,
+		latitude: fallbackLatitude,
+		longitude: fallbackLongitude
 	}
 }
 
