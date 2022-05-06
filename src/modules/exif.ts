2c197f5b726278c2c8e00ca0750dbacae5608dda
Scrounger <scrounger@gmx.net>
2022-05-06 10:25:01 +0200

    exif.ts error handling
    check if values available -> prevent throwing an error

--- src/modules/exif.ts
+++ src/modules/exif.ts
@@ -1,22 +1,30 @@
 import * as exifr from "exifr";
 import { GlobalHelper } from "./global-helper"
 
-export interface exifinfo{
+export interface exifinfo {
 	info1: string;
 	info2: string;
 	info3: string;
-	date: Date;
+	date: Date | null;
 	latitude: number | null;
 	longitude: number | null;
 }
 
-export async function getPictureInformation(Helper: GlobalHelper, file: string | Buffer ): Promise<exifinfo | null >{
-	try{
-		const PictureInfo = await exifr.parse(file, ["XPTitle", "XPComment", "XPSubject", "DateTimeOriginal", "latitude", "longitude"] );
+export async function getPictureInformation(Helper: GlobalHelper, file: string | Buffer): Promise<exifinfo | null> {
+	try {
+		const PictureInfo = await exifr.parse(file, ["XPTitle", "XPComment", "XPSubject", "DateTimeOriginal", "latitude", "longitude"]);
 		const GpsInfo = await exifr.gps(file);
 
-		return { info1: PictureInfo["XPTitle"] || "", info2: PictureInfo["XPSubject"] || "", info3: PictureInfo["XPComment"] || "", date: new Date (PictureInfo["DateTimeOriginal"]), latitude: GpsInfo["latitude"] || null, longitude: GpsInfo["longitude"] || null};
+		return {
+			info1: PictureInfo && PictureInfo["XPTitle"] ? PictureInfo["XPTitle"] : "",
+			info2: PictureInfo && PictureInfo["XPSubject"] ? PictureInfo["XPSubject"] : "",
+			info3: PictureInfo && PictureInfo["XPComment"] ? PictureInfo["XPComment"] : "",
+			date: PictureInfo && PictureInfo["DateTimeOriginal"] ? new Date(PictureInfo["DateTimeOriginal"]) : null,
+			latitude: GpsInfo && GpsInfo.latitude ? GpsInfo.latitude : null,
+			longitude: GpsInfo && GpsInfo.longitude ? GpsInfo.longitude : null
+		};
 	} catch (error) {
+		Helper.ReportingError(error as Error, "Unknown Error", "exif", "getPictureInformation");
 		return null;
 	}
 }
