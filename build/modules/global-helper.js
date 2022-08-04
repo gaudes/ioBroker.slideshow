39c79833abb3858a26a42ace73d15d0ae54ff47e
gaudes <ralf@gaudes.net>
2022-08-04 13:47:58 +0200

    Build

--- build/modules/global-helper.js
+++ build/modules/global-helper.js
@@ -1,8 +1,6 @@
-var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
@@ -17,9 +15,6 @@ var __reExport = (target, module2, copyDefault, desc) => {
   }
   return target;
 };
-var __toESM = (module2, isNodeMode) => {
-  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", !isNodeMode && module2 && module2.__esModule ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
-};
 var __toCommonJS = /* @__PURE__ */ ((cache) => {
   return (module2, temp) => {
     return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
@@ -29,7 +24,6 @@ var global_helper_exports = {};
 __export(global_helper_exports, {
   GlobalHelper: () => GlobalHelper
 });
-var SentryObj = __toESM(require("@sentry/types"));
 class GlobalHelper {
   constructor(adapterInstance) {
     this.Adapter = adapterInstance;
@@ -54,7 +48,7 @@ class GlobalHelper {
     try {
       if (this.Sentry && this.Adapter.config.sentry_disable === false && ReportSentry === true) {
         this.Sentry && this.Sentry.withScope((scope) => {
-          scope.setLevel(SentryObj.Severity.Error);
+          scope.setLevel("error");
           scope.setExtra("NameFunction", NameFunction);
           scope.setExtra("NameAction", NameAction);
           if (Info) {
