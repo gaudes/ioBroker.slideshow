a55cd423a15d82199748b8b7c6cb47329401df6e
gaudes <ralf@gaudes.net>
2022-08-04 11:19:49 +0200

    Fix

--- src/modules/slideLocal.ts
+++ src/modules/slideLocal.ts
@@ -52,7 +52,7 @@ export async function updatePictureList(Helper: GlobalHelper): Promise<LocalPict
 		} else {
 			await Promise.all(CurrentImageFiles.map(async file => {
 				const CurrentImageFile = await Helper.Adapter.readFileAsync("vis.0", `/slideshow/${file.file}`);
-				const fileInfo = await getPictureInformation(Helper, CurrentImageFile.file);
+				const fileInfo = await getPictureInformation(Helper, CurrentImageFile.data);
 				let info1, info2, info3 = "";
 				let date = null;
 				fileInfo?.info1 ? info1 = fileInfo?.info1 : info1 = "";
